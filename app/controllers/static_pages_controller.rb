55ebdf627218dcd0be62250a4056e1f53f63f63b
Philippe Clauzel <philippe.clauzel@sage.com>
2017-02-19 20:58:58 +0000

    Add classes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,62 @@
 class StaticPagesController < ApplicationController
   def home
   end
+
+  def classes
+  end
+
+  def ab_solution
+  end
+
+  def bootcamp
+  end
+
+  def boxing
+  end
+
+  def circuit_training
+  end
+
+  def cycle_fit
+  end
+
+  def cycle_pump
+  end
+
+  def hiit
+  end
+
+  def kettle_x_fit
+  end
+
+  def lbt
+  end
+
+  def mma
+  end
+
+  def pilates
+  end
+
+  def pilates_hiit
+  end
+
+  def pumped
+  end
+
+  def rig_assault
+  end
+
+  def s_and_c
+  end
+
+  def total_body
+  end
+
+  def trx
+  end
+
+  def ultimate_endurance
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+
+  get '/classes',                     to: 'static_pages#classes'
+  get '/classes/ab-solution',         to: 'static_pages#ab_solution'
+  get '/classes/bootcamp',            to: 'static_pages#bootcamp'
+  get '/classes/boxing',              to: 'static_pages#boxing'
+  get '/classes/circuit-training',    to: 'static_pages#circuit_training'
+  get '/classes/cycle-fit',           to: 'static_pages#cycle_fit'
+  get '/classes/cycle-pump',          to: 'static_pages#cycle_pump'
+  get '/classes/hiit',                to: 'static_pages#hiit'
+  get '/classes/kettle-x-fit',        to: 'static_pages#kettle_x_fit'
+  get '/classes/lbt',                 to: 'static_pages#lbt'
+  get '/classes/mma',                 to: 'static_pages#mma'
+  get '/classes/pilates',             to: 'static_pages#pilates'
+  get '/classes/pilates-hiit',        to: 'static_pages#pilates_hiit'
+  get '/classes/pumped',              to: 'static_pages#pumped'
+  get '/classes/rig-assault',         to: 'static_pages#rig_assault'
+  get '/classes/s-and-c',             to: 'static_pages#s_and_s'
+  get '/classes/total-body',          to: 'static_pages#total_body'
+  get '/classes/trx',                 to: 'static_pages#trx'
+  get '/classes/ultimate-endurance',  to: 'static_pages#ultimate_endurance'
+
+  get '/facilities',                  to: 'static_pages#facilities'
+
+  #get '/classes/:class', to: 'classes#show'
 end
