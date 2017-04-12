7d794d7899637fed70e6fa346423885c79e6439c
Philippe Clauzel <philippe.clauzel@sage.com>
2017-04-12 21:26:53 +0100

    Add user logins

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  scope '/backend' do
+    resources :users
+  end
+
+  #get  '/signup',  to: 'users#new'
+  #post '/signup',  to: 'users#create'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
   root 'static_pages#home'
 
   get '/classes',                     to: 'static_pages#classes'
