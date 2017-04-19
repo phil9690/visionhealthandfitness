a686f580b436d50a1c3c2389274708526ad8335a
Philippe Clauzel <philippe.clauzel@sage.com>
2017-04-19 18:07:05 +0100

    Update styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  layout 'backend/backend'
+  #layout 'backend/backend'
 
   before_action :logged_in_user
   #before_action :correct_user, only: [:edit, :update]
