6d29a7453b8138e9fa243f132665f1197969b5e6
Philippe Clauzel <philippe.clauzel@sage.com>
2018-02-15 12:59:20 +0000

    Update timetable

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
 
   #get  '/signup',  to: 'users#new'
   #post '/signup',  to: 'users#create'
- 
+
   # Sessions
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
